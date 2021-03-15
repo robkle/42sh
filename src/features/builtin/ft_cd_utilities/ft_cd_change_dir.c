f233f3ad9a1deca2e57046a0559536549b6eafa9
ihwang <tango@hwang-intaegs-MacBook-Pro.local>
2021-03-15 22:20:33 +0200

    [RESOLVE]#29 Fix cd to create new OLDPWD or PWD when it doesn't have them.

--- src/features/builtin/ft_cd_utilities/ft_cd_change_dir.c
+++ src/features/builtin/ft_cd_utilities/ft_cd_change_dir.c
@@ -6,7 +6,7 @@
 /*   By: ihwang <ihwang@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/02 17:45:50 by ihwang            #+#    #+#             */
-/*   Updated: 2021/02/02 17:46:12 by ihwang           ###   ########.fr       */
+/*   Updated: 2021/03/15 22:19:00 by ihwang           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,29 +16,46 @@
 **	step10 in posix doc
 */
 
+static void ft_cd_set_env(char *name, char *value)
+{
+	char **av;
+
+	av = (char**)malloc(sizeof(char*) * 3);
+	av[0] = (char*)malloc(ft_strlen("export") + 1);
+	av[1] = (char*)malloc(ft_strlen(name) + ft_strlen(value) + 2);
+	av[2] = NULL;
+	ft_strcpy(av[0], "export");
+	ft_strcpy(av[1], name);
+	ft_strcat(av[1], "=");
+	ft_strcat(av[1], value);
+	ft_export(2, av);
+	ft_strlst_del(&av, 2);
+}
+
 static void	manipulate_var(t_cd *cd)
 {
-	char 	*pwd;
-	char	*oldpwd;
 	char	absolute_path[PATH_MAX];
 
-	if ((pwd = ft_getenv("PWD")) && (oldpwd = ft_getenv("OLDPWD")))
-		ft_strcpy(oldpwd, pwd);
+	if (ft_getenv("PWD") != NULL)
+		ft_cd_set_env("OLDPWD", ft_getenv("PWD"));
+	else
+		ft_cd_set_env("OLDPWD", "");
+	getcwd(absolute_path, sizeof(absolute_path));
 	if (!(cd->opt.applied & BUILTIN_CD_OPT_P))
 	{
-		if (pwd && cd->prev_curpath)
-			ft_strcpy(pwd, cd->prev_curpath);
-		else if (pwd && cd->curpath)
-			ft_strcpy(pwd, cd->curpath);
+		if (cd->prev_curpath)
+			ft_cd_set_env("PWD", cd->prev_curpath);
+		else if (cd->curpath)
+			ft_cd_set_env("PWD", cd->curpath);
 	}
 	else
 	{
 		ft_memset((void*)absolute_path, 0, sizeof(absolute_path));
-		if (getcwd(absolute_path, sizeof(absolute_path)) != NULL && pwd)
-			ft_strcpy(pwd, absolute_path);
+		if (getcwd(absolute_path, sizeof(absolute_path)) != NULL && ft_getenv("PWD"))
+			ft_cd_set_env("PWD", absolute_path);
 	}
-    if (is_root_dir(pwd))
-        ft_strcpy(pwd, "/");
+    if (is_root_dir(ft_getenv("PWD")))
+		ft_cd_set_env("PWD", "/");
 }
 
 int ft_cd_change_dir(t_cd *cd)
