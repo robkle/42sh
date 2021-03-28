2ee6b8eb09068e104373e45b56b8e7f66b6b9203
ihwang <tango@hwangintaegsMBP.lan>
2021-03-28 20:36:16 +0300

    Fix temp

--- src/cores/line_edition/up_down_key.c
+++ src/cores/line_edition/up_down_key.c
@@ -6,7 +6,7 @@
 /*   By: ihwang <ihwang@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/04/20 00:21:14 by ihwang            #+#    #+#             */
-/*   Updated: 2021/03/19 21:57:11 by rklein           ###   ########.fr       */
+/*   Updated: 2021/03/28 20:36:09 by ihwang           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,8 +34,8 @@ static void	up_down_key_apply_statuses(t_l *l)
 	apply_termcap_str("cd", 0, 0);
 	l->starting_row = new_starting_row;
 	ft_putstr(l->line);
-	if (l->x == 0)
-		ft_putchar('\n');
+	// if (l->x == 0)//OLD
+		// ft_putchar('\n');//OLD
 }
 
 int			up_key(t_l *l)
@@ -48,7 +48,8 @@ int			up_key(t_l *l)
 	g_shell.history->hst--;
 	ft_strdel(&l->line);
 	len = ft_strlen(g_shell.history->hist[g_shell.history->hst]);
-	d = g_shell.history->hist[g_shell.history->hst][len - 1] == 4 ? 2 : 1;
+	// d = g_shell.history->hist[g_shell.history->hst][len - 1] == 4 ? 2 : 1;//OLD
+	d = 1;//NEW
 	l->line = ft_strndup(g_shell.history->hist[g_shell.history->hst], \
 	len - d);
 	up_down_key_apply_statuses(l);
@@ -68,7 +69,8 @@ static void	down_key(t_l *l)
 	if (g_shell.history->hst != g_shell.history->curr)
 	{
 		len = ft_strlen(g_shell.history->hist[g_shell.history->hst]);
-		d = g_shell.history->hist[g_shell.history->hst][len - 1] == 4 ? 2 : 1;
+		// d = g_shell.history->hist[g_shell.history->hst][len - 1] == 4 ? 2 : 1;//OLD
+		d = 1;//NEW
 		l->line = ft_strndup(g_shell.history->hist[g_shell.history->hst], \
 		len - d);
 	}
